1cbd42f63e611eb233c0d306c6320b542453aa16
Valerio <simon.valerio@etu.univ-poitiers.fr>
2022-11-22 14:21:21 +0100

    ex 2 final

--- simon~1.sql
+++ simon~1.sql
@@ -1,15 +1,47 @@
---exercice 2--
+-- Exercice 2 : requêtes simples --
+-- 1 --
 
-select TITLE from movie where director = 'Steven Spielberg';
+Select *
+From MOVIE
+where DIRECTOR = 'Steven Spielberg';
 
-select movie.YEAR, rating.STARS from rating, movie where movie.mID=rating.mID and stars=4 or stars=5 order by movie.YEAR;
+-- 2 --
 
-select reviewer.name from rating, reviewer, movie where reviewer.RID=rating.RID and movie.mID=rating.mID and movie.title='Gone with the Wind';
+Select m.year 
+from rating r, movie m
+where r.mID = m.mID and r.stars >= 4
+order by m.year;
 
-select movie.director, movie.title, rating.STARS from movie, reviewer, rating where movie.director=reviewer.name and reviewer.RID=rating.RID;
+-- 3 --
 
-select reviewer.name, movie.title, rating.stars from rating, reviewer, movie where reviewer.RID=rating.RID and movie.mID=rating.mID order by reviewer.NAME, movie.TITLE, rating.STARS DESC;
+select distinct (name)
+from reviewer re, rating r, movie m
+where re.rID = r.rID and r.mID = m.mID 
+and m.title = 'Gone with the Wind';
 
+-- 4 --
+
+select re.name, m.title, r.stars
+from reviewer re, movie m, rating r
+where re.rID = r.rID and r.mID = m.mID and m.director = re.name;
+
+-- 5 --
+
+select stars, ratingdate
+from reviewer re, movie m, rating r
+where re.rID = r.rID and r.mID = m.mID
+order by re.name, m.title, r.stars;
+
+-- 6 --
+
+select distinct(title)
+from movie 
+where title NOT IN (select m.title from movie m, reviewer re, rating r
+where re.rID = r.rID 
+and r.mID = m.mID 
+and re.name = 'Chris Jackson');
+
+-- 7 --
 
 select re.name, m.title
 from rating r1, rating r2, movie m, reviewer re
@@ -19,10 +51,14 @@ and r1.mID = r2.mID
 and r1.rID = r2.rID
 and r1.ratingdate < r2.ratingdate
 and r1.stars < r2.stars; 
---8--
+
+-- 8 --
+
 select distinct re1.name, re2.name
 from rating r1, rating r2, reviewer re1, reviewer re2
 where re2.rID = r2.rID
 and re1.rID = r1.rID
 and r1.mID = r2.mID
 and re1.name > re2.name;
+
+
